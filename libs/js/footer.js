a7bbf35e7ca89a785be9f11f72780a88299d4297
leaqi <qifzl@qq.com>
2022-05-28 11:45:27 +0800

    Update

---
+++ libs/js/footer.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?01b53120b6a90d4329c23ce398ecc946";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
